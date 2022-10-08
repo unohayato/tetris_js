39c7a4d08c3959a63237750dcd2e168c9e9375b3
unohayato <mamidori1p1p@gmail.com>
2022-10-09 02:06:12 +0900

    drawBlock 関数

--- js/render.js
+++ js/render.js
@@ -3,3 +3,9 @@ let ctx = canvas.getContext('2d');
 let W = 300, H = 600;
 let BLOCK_W = W / COLS, BLOCK_H = H / ROWS;
 
+// draw a single square at (x, y)
+function drawBlock(x, y){
+  ctx.fillRect(BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1, BLOCK_H- 1);
+  ctx.strokeRect(BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1, BLOCK_H- 1);
+
+}
